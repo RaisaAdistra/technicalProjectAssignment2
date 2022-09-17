46b6310d93c7f0e7f2ca7ec86f3efb43d43b6296
RaisaAdistra <rrsasikirana@gmail.com>
2022-09-17 08:58:05 +0700

    add layout html

---
+++ script.js
@@ -0,0 +1,22 @@
+//exercise 1
+let num1=10
+let num2=5
+
+if (num1 >= num2) {
+  console.log(num1)
+}
+else {
+  console.log(num2)
+}
+
+//exercise 2
+function tiketNonton(hargaTiket, jumlahTiket){
+    let hargaTotal=0
+  
+    hargaTotal=hargaTiket * jumlahTiket
+    return hargaTotal
+  }
+  
+  console.log(tiketNonton(50000, 2))
+
+//exercise 3
